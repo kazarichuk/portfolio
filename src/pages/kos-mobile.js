5133b10824822ef413f24ad855237ab46db2bcfb
Kos <kazarichuk@mcookie.io>
2023-07-14 15:07:07 +0200

    Initial commit

--- src/pages/kos-mobile.js
+++ src/pages/kos-mobile.js
@@ -1,6 +1,28 @@
-import { useCallback } from "react";
+import React, { useState, useCallback } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { gsap } from "gsap";
 import styles from "./kos-mobile.module.css";
+import projectsImage from "../assets-mob/Mobile-Projects.webp";
+import codeImage from "../assets-mob/Mobile-Code.webp";
+import designImage from "../assets-mob/Mobile-Design.webp";
+import managementImage from "../assets-mob/Mobile-Managment.webp";
+
 const KosMobile = () => {
+  const [activeButton, setActiveButton] = useState(null);
+  const [showProjects, setShowProjects] = useState(true);
+  const [showCode, setShowCode] = useState(false);
+  const [showDesign, setShowDesign] = useState(false);
+  const [showManagementSupport, setShowManagement] = useState(false);
+  
+  const handleButtonClick = useCallback((buttonName) => {
+    setActiveButton(buttonName);
+    setShowProjects(buttonName === "projects");
+    setShowCode(buttonName === "code");
+    setShowDesign(buttonName === "design");
+    setShowManagement(buttonName === "managementSupport");
+  }, []);
+
+
   const onBottomNavItemsClick = useCallback(() => {
     const anchor = document.querySelector(
       "[data-scroll-to='craftingInteractiveDesigns']"
@@ -38,7 +60,21 @@ const KosMobile = () => {
     if (anchor) {
       anchor.scrollIntoView({ block: "start", behavior: "smooth" });
     }
+    
+    // GSAP animation timeline
+    const tl = gsap.timeline();
+
+    // Button pulse animation
+    tl.to(".primaryButton1", {
+      scale: 1.2,
+      duration: 0.3,
+      ease: "power1.inOut",
+      yoyo: true,
+      repeat: 1,
+    });
   }, []);
+
+  
 
   return (
     <div className={styles.kosMobile}>
@@ -66,7 +102,9 @@ const KosMobile = () => {
           <div
             className={styles.craftingInteractiveDesigns}
             data-scroll-to="craftingInteractiveDesigns"
-          >{`Crafting Interactive Designs for Web & Mobile`}</div>
+          >
+            Crafting Interactive Designs for Web & Mobile
+          </div>
           <div className={styles.frameParent}>
             <div className={styles.aSeniorUxDesignerWithASpWrapper}>
               <div className={styles.aSeniorUx}>
@@ -121,51 +159,127 @@ const KosMobile = () => {
         </div>
       </div>
       <div className={styles.windowdefoult}>
-        <div className={styles.window}>
+      <div className={styles.window}>
+        {!showProjects && (
           <div className={styles.text1}>
-            <div className={styles.versatileSkillSet}>
+            <div
+              className={`${styles.versatileSkillSet} ${
+                showProjects ? styles.hidden : ""
+              }`}
+            >
               Versatile Skill Set for Comprehensive Solutions
             </div>
-            <div className={styles.blendingDesignCoding}>
-              Blending design, coding, project management, and support to
-              deliver exceptional results across the digital landscape
+            <div
+              className={`${styles.blendingDesignCoding} ${
+                showProjects ? styles.hidden : ""
+              }`}
+            >
+              Blending design, coding, project management, and support to deliver exceptional results across the digital landscape
             </div>
           </div>
-        </div>
-        <div className={styles.controls}>
-          <img
-            className={styles.windowResizeLeading}
-            alt=""
-            src="/window-resize--leading.svg"
-          />
-          <img
-            className={styles.windowResizeTrailing}
-            alt=""
-            src="/window-resize--trailing.svg"
-          />
-          <div className={styles.windowButtons}>
-            <div className={styles.box1}>
-              <button className={styles.buttons}>
-                <div className={styles.projects}>Projects</div>
-              </button>
-              <button className={styles.buttons}>
-                <div className={styles.code}>Code</div>
-              </button>
-            </div>
-            <div className={styles.box1}>
-              <button className={styles.buttons}>
-                <div className={styles.design}>Design</div>
-              </button>
-              <button className={styles.buttons}>
-                <div className={styles.managementSupportContainer}>
-                  <p className={styles.management}>{`Management & `}</p>
-                  <p className={styles.management}>Support</p>
-                </div>
-              </button>
+        )}
+
+        {showProjects && (
+          <div className={`${styles.images} ${styles.imageContainer}`}>
+            <LazyLoadImage
+              className={`${styles.image} ${styles.projectsImage}`}
+              src={projectsImage}
+              alt="Projects"
+              effect="blur"
+            />
+          </div>
+        )}
+
+        {showCode && (
+          <div className={`${styles.images} ${styles.imageContainer}`}>
+            <LazyLoadImage
+              className={`${styles.image} ${styles.codeImage}`}
+              src={codeImage}
+              alt="Code"
+              effect="blur"
+            />
+          </div>
+        )}
+
+        {showDesign && (
+          <div className={`${styles.images} ${styles.imageContainer}`}>
+            <LazyLoadImage
+              className={`${styles.image} ${styles.designImage}`}
+              src={designImage}
+              alt="Design"
+              effect="blur"
+            />
+          </div>
+        )}
+
+        {showManagementSupport && (
+          <div className={`${styles.images} ${styles.imageContainer}`}>
+            <LazyLoadImage
+              className={`${styles.image} ${styles.managementImage}`}
+              src={managementImage}
+              alt="Management & Support"
+              effect="blur"
+            />
+          </div>
+        )}
+      </div>
+      <div className={styles.controls}>
+        <img
+          className={styles.windowResizeLeading}
+          alt=""
+          src="/window-resize--leading.svg"
+        />
+        <img
+          className={styles.windowResizeTrailing}
+          alt=""
+          src="/window-resize--trailing.svg"
+        />
+        <div className={styles.windowButtons}>
+          <div className={styles.box1}>
+            <button
+              className={`${styles.buttons} ${
+                activeButton === "projects" ? styles.activeButton : ""
+              }`}
+              onClick={() => handleButtonClick("projects")}
+            >
+              <div className={styles.projects}>Projects</div>
+            </button>
+            <button
+              className={`${styles.buttons} ${
+                activeButton === "code" ? styles.activeButton : ""
+              }`}
+              onClick={() => handleButtonClick("code")}
+            >
+              <div className={styles.code}>Code</div>
+            </button>
+          </div>
+          <div className={styles.box1}>
+            <button
+              className={`${styles.buttons} ${
+                activeButton === "design" ? styles.activeButton : ""
+              }`}
+              onClick={() => handleButtonClick("design")}
+            >
+              <div className={styles.design}>Design</div>
+            </button>
+            <button
+              className={`${styles.buttons} ${
+                activeButton === "managementSupport" ? styles.activeButton : ""
+              }`}
+              onClick={() => handleButtonClick("managementSupport")}
+            >
+              <div className={styles.managementSupportContainer}>
+                <p className={styles.management}>Management &</p>
+                <p className={styles.management}>Support</p>
+              </div>
+            </button>
             </div>
           </div>
         </div>
       </div>
+
+
+
       <div className={styles.whatIAmLookingForParent}>
         <div className={styles.whatIAm}>What I am looking for</div>
         <div className={styles.imInSearch}>
@@ -174,10 +288,7 @@ const KosMobile = () => {
           contribute to and celebrate remarkable achievements together while
           enjoying a variety of challenges to keep things stimulating.
         </div>
-        <div
-          className={styles.moonpicture}
-          data-scroll-to="moonPictureContainer"
-        >
+        <div className={styles.moonpicture} data-scroll-to="moonPictureContainer">
           <div className={styles.moonshadow} />
           <div className={styles.moonframe}>
             <img className={styles.moonIcon} alt="Moon" src="/moon.svg" />
@@ -186,11 +297,7 @@ const KosMobile = () => {
         <div className={styles.boxesLooking}>
           <div className={styles.scroll}>
             <div className={styles.ellipseParent}>
-              <img
-                className={styles.frameItem}
-                alt=""
-                src="/ellipse-6@2x.png"
-              />
+              <img className={styles.frameItem} alt="" src="/ellipse-6@2x.png" />
               <div className={styles.frameInner} />
               <div className={styles.socioDemographicsAnalystPs}>
                 Socio-demographics analyst, psychology enthusiast, digital
@@ -199,11 +306,7 @@ const KosMobile = () => {
               <div className={styles.growth}>Growth</div>
             </div>
             <div className={styles.ellipseParent}>
-              <img
-                className={styles.frameItem}
-                alt=""
-                src="/ellipse-61@2x.png"
-              />
+              <img className={styles.frameItem} alt="" src="/ellipse-61@2x.png" />
               <div className={styles.frameInner} />
               <div className={styles.socioDemographicsAnalystPs}>
                 Socio-demographics analyst, psychology enthusiast, digital
@@ -212,11 +315,7 @@ const KosMobile = () => {
               <div className={styles.growth}>Team Success</div>
             </div>
             <div className={styles.ellipseParent}>
-              <img
-                className={styles.frameItem}
-                alt=""
-                src="/ellipse-62@2x.png"
-              />
+              <img className={styles.frameItem} alt="" src="/ellipse-62@2x.png" />
               <div className={styles.frameInner} />
               <div className={styles.analystsDesignersPsycholog}>
                 Analysts, designers, psychologists involved in creating Life
@@ -234,21 +333,31 @@ const KosMobile = () => {
             Design with love © Kazarichuk 2023. All right reserved
           </div>
           <img className={styles.lineIcon} alt="" src="/line.svg" />
+
           <div className={styles.box}>
             <img className={styles.logoIcon} alt="" src="/logo.svg" />
             <div className={styles.icons}>
-              <a className={styles.icons1}>
-                <img className={styles.vectorIcon} alt="" src="/vector.svg" />
-                <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
-                <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
-              </a>
-              <img className={styles.icons2} alt="" src="/icons.svg" />
-              <img className={styles.icons2} alt="" src="/icons1.svg" />
-              <img className={styles.icons2} alt="" src="/icons2.svg" />
+
+          <a href="https://www.instagram.com/konstantin_kazarichuk/" className={styles.icons1} target="_blank">
+            <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+            <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+            <img className={styles.vectorIcon2} alt="" src="/vector3.svg" />
+          </a>
+          <a href="https://github.com/kazarichuk" target="_blank">
+            <img className={styles.icons2} alt="" src="/icons3.svg" />
+          </a>
+          <a href="https://www.linkedin.com/in/konstantin-k-1124791a0/" target="_blank">
+            <img className={styles.icons2} alt="" src="/icons1.svg" />
+          </a>
+          <a href="https://medium.com/@kazarichuk" target="_blank">
+            <img className={styles.icons2} alt="" src="/icons4.svg" />
+            </a>
             </div>
           </div>
         </div>
       </footer>
+
+      
       <div className={styles.frame01}>
         <img
           className={styles.mainIcon}
@@ -257,36 +366,24 @@ const KosMobile = () => {
           src="/main@3x.png"
         />
         <b className={styles.h1Title}>Kazarichuk Design</b>
-        <div
-          className={styles.h2Subtitle}
-        >{`Crafting Digital Experiences with Precision & Passion`}</div>
+        <div className={styles.h2Subtitle}>
+          Crafting Digital Experiences with Precision & Passion
+        </div>
         <img className={styles.ellipsesIcon} alt="" src="/frame-625959.svg" />
         <nav className={styles.bottomNavigation}>
-          <div
-            className={styles.bottomNavItems}
-            onClick={onBottomNavItemsClick}
-          >
+          <div className={styles.bottomNavItems} onClick={onBottomNavItemsClick}>
             <img className={styles.sportIcon} alt="" src="/sport.svg" />
             <div className={styles.name}>Creations</div>
           </div>
-          <div
-            className={styles.bottomNavItems}
-            onClick={onBottomNavItems1Click}
-          >
+          <div className={styles.bottomNavItems} onClick={onBottomNavItems1Click}>
             <img className={styles.travelIcon} alt="" src="/travel.svg" />
             <div className={styles.name}>Expertise</div>
           </div>
-          <div
-            className={styles.bottomNavItems}
-            onClick={onBottomNavItems2Click}
-          >
+          <div className={styles.bottomNavItems} onClick={onBottomNavItems2Click}>
             <img className={styles.travelIcon} alt="" src="/maps.svg" />
             <div className={styles.name}>Behind</div>
           </div>
-          <div
-            className={styles.bottomNavItems}
-            onClick={onBottomNavItems3Click}
-          >
+          <div className={styles.bottomNavItems} onClick={onBottomNavItems3Click}>
             <img className={styles.travelIcon} alt="" src="/mail.svg" />
             <div className={styles.name}>Connect</div>
           </div>
@@ -294,17 +391,17 @@ const KosMobile = () => {
       </div>
       <div className={styles.textH6Parent}>
         <div className={styles.textH6}>
-          <span>{`Delve deeper into my professional journey. Explore my comprehensive `}</span>
+          <span>Delve deeper into my professional journey. Explore my comprehensive </span>
           <span className={styles.figmaPortfolio}>Figma portfolio</span>
-          <span>{` and access my detailed `}</span>
+          <span> and access my detailed </span>
           <span className={styles.figmaPortfolio}>PDF Resume below</span>
           <span>. Let's embark on a design adventure together.</span>
         </div>
         <div className={styles.buttons4}>
-          <button className={styles.primaryButton}>
-            <b className={styles.label}>All Projects in Figma</b>
-          </button>
-          <button className={styles.outlinedButton}>
+        <button className={styles.primaryButton} onClick={() => window.open("https://www.figma.com/proto/AFRgUvVtcrWOxuvRgeqngO/Portfolio-v2?node-id=0-1&viewport=1194%2C1051%2C0.14&scaling=min-zoom&starting-point-node-id=1%3A2809", "_blank")} >
+        <b className={styles.label}>All Projects in Figma</b>
+      </button>
+          <button className={styles.outlinedButton}onClick={() => window.open("https://drive.google.com/file/d/1VdatcOYUnK2xEqK4h3MtMRAV3xgQZvkj/view?usp=sharing ", "_blank")} >
             <b className={styles.label1}>PDF Resume</b>
           </button>
         </div>
@@ -325,6 +422,7 @@ const KosMobile = () => {
               className={styles.email}
               href="mailto:kazarichuk@gmail.com"
               target="_blank"
+              rel="noopener noreferrer"
             >
               kazarichuk@gmail.com
             </a>
